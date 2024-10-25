d387aa38b006afbd2ce6f4aa51045908fa330f35
Ben Trainor <8466944+ben-trainor-1@users.noreply.github.com>
2024-10-25 14:07:17 -0500

    new front page layout and color adjustments

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 $(document).ready(() => {
 
-    $("#menu a").on("click", function() { return false; });
-    $("#menu a").on("click", function(event) { 
+    $("#header a").on("click", function() { return false; });
+    $("#header a").on("click", function(event) { 
         let href = event.target.href;
         $("#content").load(href.substring(href.lastIndexOf("\/") + 1));
     });
 
     $("#content").load("about.html");
     $("#footer").load("social.html");
-    
+
 });
