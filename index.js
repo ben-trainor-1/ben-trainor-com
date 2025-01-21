ecd24c77619528c757baf613ac7531ad2784d595
Ben Trainor <8466944+ben-trainor-1@users.noreply.github.com>
2025-01-21 13:45:46 -0600

    gettin real close

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ $(document).ready(() => {
 
     $("#header a").on("click", function() { return false; });
     $("#header a").on("click", function(event) { 
+        killCurrentSounds();
         let href = event.target.href;
         $("#content").load(href.substring(href.lastIndexOf("\/") + 1));
-        killCurrentSounds();
+        $("html").scrollTop(0);
     });
 
     $("#content").load("about.html");
